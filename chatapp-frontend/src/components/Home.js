794c9a3a03b1ec18186af68b2c2e57b8799f7b6e
Fares Khalil <65375081+1FaresKhalil@users.noreply.github.com>
2023-05-05 18:59:41 +0300

    login issue

--- chatapp-frontend/src/components/Home.js
+++ chatapp-frontend/src/components/Home.js
@@ -1,81 +1,79 @@
-import React, {useEffect} from 'react';
-import '../style/_home.scss'
+import React, { useEffect } from "react";
+import "../style/_home.scss";
 import Header from "./Header";
-import {Button, Container, Table} from "react-bootstrap";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchAllUsers} from "./slices/userSlice";
+import { Button, Container, Table } from "react-bootstrap";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchAllUsers } from "./slices/userSlice";
 import Spinner from "./Spinner";
+import axios from "axios";
 import UnAuthenticated from "./UnAuthenticated";
 
+let token = JSON.parse(localStorage.getItem("token"));
+
 function Users() {
-    let dispatch = useDispatch();
-    let users = useSelector(state => state.user.users);
+  let [users, setUsers] = React.useState([]);
 
-    useEffect(() => {
-        dispatch(fetchAllUsers())
-        document.body.style.backgroundImage = 'none';
-        document.body.style.display = 'block';
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get(`http://localhost:8000/users`, {
+        headers: { Authorization: token },
+      });
+      setUsers(res.data.users);
+    };
+    fetchUsers();
+    document.body.style.backgroundImage = "none";
+    document.body.style.display = "block";
 
-        return () => {
-            document.body.style.backgroundImage = '';
-            document.body.style.display = '';
-        };
+    return () => {
+      document.body.style.backgroundImage = "";
+      document.body.style.display = "";
+    };
+  }, []);
 
-    }, []);
-
-    if(localStorage.getItem('token') === null){
-        return <UnAuthenticated />
-    }
-    if(localStorage.getItem('token') !== null){
-        return (
-            <div className='home'>
-                <Header/>
-                <Container>
-                    {
-                        users.length !== 0?
-                            <>
-                                <h2 className='pt-3 pb-3 subTitle'>Users</h2>
-                                <Table hover responsive>
-                                    <thead>
-                                    <tr className='text-center'>
-                                        <th>ID</th>
-                                        <th>Name</th>
-                                        <th>Username</th>
-                                        <th>Age</th>
-                                        <th>Chat</th>
-                                    </tr>
-                                    </thead>
-                                    {
-                                        users?.map((user) => (
-                                            <tbody key={user._id}>
-                                            <tr>
-                                                <td className='p-4 text-center'>{user._id}</td>
-                                                <td className='p-4 text-center'>{user.name}</td>
-                                                <td className='p-4 text-center'>{user.username}</td>
-                                                <td className='p-4 text-center'>{user.age}</td>
-                                                <td className='p-4'>
-                                                    <div className='d-flex flex-row justify-content-center'>
-                                                        <Button className='chat text-white' type="button">
-                                                            Start
-                                                        </Button>
-                                                    </div>
-
-                                                </td>
-
-                                            </tr>
-                                            </tbody>
-                                        ))
-                                    }
-                                </Table>
-
-                            </>
-                            :
-                            <Spinner/>
-                    }
-                </Container>
-            </div>
-        );
-    }
+  if (localStorage.getItem("token") === null) {
+    return <UnAuthenticated />;
+  }
+  if (users?.length > 0) {
+    return (
+      <div className="home">
+        <Header />
+        <Container>
+          <>
+            <h2 className="pt-3 pb-3 subTitle">Users</h2>
+            <Table hover responsive>
+              <thead>
+                <tr className="text-center">
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Username</th>
+                  <th>Age</th>
+                  <th>Chat</th>
+                </tr>
+              </thead>
+              {users?.map((user) => (
+                <tbody key={user._id}>
+                  <tr>
+                    <td className="p-4 text-center">{user._id}</td>
+                    <td className="p-4 text-center">{user.name}</td>
+                    <td className="p-4 text-center">{user.username}</td>
+                    <td className="p-4 text-center">{user.age}</td>
+                    <td className="p-4">
+                      <div className="d-flex flex-row justify-content-center">
+                        <Button className="chat text-white" type="button">
+                          Start
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </>
+        </Container>
+      </div>
+    );
+  }
+  return <Spinner />;
 }
 
 export default Users;
